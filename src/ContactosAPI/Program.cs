a9eb3edd9b976d0f7b64c51274e59a624383cf34
Fernanda Rivera <e.riveraluisa2017@gmail.com>
2025-08-02 10:16:52 -0600

    Modificando el archivo program

--- src/ContactosAPI/Program.cs
+++ src/ContactosAPI/Program.cs
@@ -43,4 +43,5 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
 
+//este es un comentario, hola key
 public partial class Program { }
